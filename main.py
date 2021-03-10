67903040d1cbd19b907ada9d2e9cc0f92adb5f74
Kaio Gama <kaioeduardoescar@gmail.com>
2021-03-10 19:50:07 -0400

    initial  2

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqpars
 
 app = Flask(__name__)
-api = Api(app) 
+api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
-    def post(Self):
-        return {"data": "Posted"}
-        
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", tyoer=str, help="Name of the video")
+video_put_args.add_argument("views", tyoer=str, help="Views of the video")
+video_put_args.add_argument("likes", tyoer=str, help="Likes of the video")
 
 
-api.add_resource(HelloWorld, "/helloworld")
+videos = {}
+
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        print(request.form['likes'])
+        return
+
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
